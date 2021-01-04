86e2c23931aed2d6eee7adc44c5b6ae6c3097716
nagalakshmi-raja-r-hcl-p <nagalakshmi.raja@hcl.com>
2021-01-04 16:02:37 +0530

    Initial commit

---
+++ src/test/java/com/example/insurance/demoinsuranceservice/DemoInsuranceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insurance.demoinsuranceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInsuranceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
